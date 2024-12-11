b0ca9d8738c5aec7fe791f3d22ffbf2c16f26cfd
ilies1511 <iziane@student.42heilbronn.de>
2024-12-11 11:56:58 +0100

    FRAME: add frame for pixel conversion

--- main.c
+++ main.c
@@ -39,6 +39,12 @@ struct file_content
 	u32   size;
 };
 
+typdef struct s_pixel
+{
+	u32	pixel[4]; //Pixel = 4 Bytes = 1 Byte = 8 Bites
+
+}		t_pixel;
+
 struct file_content   read_entire_file(char* filename)
 {
 	char* file_data = 0;
@@ -48,6 +54,10 @@ struct file_content   read_entire_file(char* filename)
 	{
 		struct stat input_file_stat = {0};
 		stat(filename, &input_file_stat);
+		/*
+			What: function obtains information about the file pointed to by path
+			Return Value -> succes: 0, Fail: -1
+		*/
 		file_size = input_file_stat.st_size;
 		file_data = mmap(0, file_size, PROT_READ | PROT_WRITE, MAP_PRIVATE, input_file_fd, 0);
 		close(input_file_fd);
@@ -55,8 +65,49 @@ struct file_content   read_entire_file(char* filename)
 	return (struct file_content){file_data, file_size};
 }
 
+char	*decode_msg(struct file_content file_content, size_t WIDTH, size_t HEIGHT)
+{
+	char	*decoded_msg; //Only Malloc
+	int		amountOfPixels;
+	int		i;
+	int		k;
+
+	(void)file_content;
+	printf("In decode_msg Funktion\n");
+
+	//TODO: HEIGHT & WIDTH of Msg (Data without Header)
+	/*
+		HEIGHT"
+		Amount of Pixel of the msg without Header
+	*/
+	// amountOfPixels = file_content.data - sizeof(bmp_header) / file_content.data[0];
+	i = 0;
+	while (i < HEIGHT)
+	{
+		k = 0;
+		while (k < WIDTH)
+		{
+			/*
+				Hier auf Pixel-Ebene: Hier Fnc die Pixel uebersetzt und speichert
+				FNC:
+				TODO: decode_pixel();
+			*/
+			k++;
+		}
+		i++;
+	}
+	// TODO: return (decoded_msg);
+	return ("alo\n");
+}
+
+/*
+	gcc -O0 -Wall -Wextra -Werror main.c -o decoder -lpthread
+*/
 int main(int argc, char** argv)
 {
+	char	*decoded_msg;
+
+	decoded_msg = NULL;
 	if (argc != 2)
 	{
 		PRINT_ERROR("Usage: decode <input_filename>\n");
@@ -70,5 +121,16 @@ int main(int argc, char** argv)
 	}
 	struct bmp_header* header = (struct bmp_header*) file_content.data;
 	printf("signature: %.2s\nfile_size: %u\ndata_offset: %u\ninfo_header_size: %u\nwidth: %u\nheight: %u\nplanes: %i\nbit_per_px: %i\ncompression_type: %u\ncompression_size: %u\n", header->signature, header->file_size, header->data_offset, header->info_header_size, header->width, header->height, header->number_of_planes, header->bit_per_pixel, header->compression_type, header->compressed_image_size);
+	/*
+		Msg Decoder using
+		struct file_content
+	{
+		i8*   data;
+		u32   size;
+	};
+	*/
+
+	decoded_msg = decode_msg(file_content, header->width, header->height);
+	printf("decoded_msg: %s\n", decoded_msg);
 	return 0;
 }
